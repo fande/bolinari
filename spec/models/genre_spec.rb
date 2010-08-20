a8cc9670200e1850ced0d6c9998b3002e713ba96
Gabriel Engel <gabriel@fande.com.br>
2010-08-20 11:01:24 -0300

    Corrigin o ingles

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,12 +1,12 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Genre do
-  it "should be create" do
+  it "should create" do
     genre = Genre.create(:name => "black metal")
     genre.should be_valid
   end
   
-  it "should be not create" do
+  it "should not create" do
     genre = Genre.create(:name => "black metal")
     genre .should_not be_valid
   end
