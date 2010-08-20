1c06e2060090edeaa1368a9fe72b2476a77026ca
Letícia <leticia@fande.com.br>
2010-08-20 17:43:15 -0300

    Spec de Artist

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -2,12 +2,12 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Artist do
   it 'should create' do
-    artist = Factory.create(:name, :genre_id )
+    artist = Factory.create(:artist)
     artist.should be_valid
   end
 
   it 'should not create' do
-    artist = Factory.build(:genre_id, :name => nil )
+    artist = Factory.build(:artist, :name => nil )
     artist.save
 
     artist.errors.on(:name).should == ("Can't be blank")
@@ -15,10 +15,10 @@ describe Artist do
   end
 
   it 'should have an unique name and an unique genre' do
-    artist = Factory.build(:name => 'Lorem', :genre_id => 1 )
+    artist = Factory.build(:artist, :name => 'Lorem', :genre_id => '1' )
     artist.save
 
-    artist_clone = Factory.build(:name => 'Lorem', :genre_id => 1)
+    artist_clone = Factory.build(:artist, :name => 'Lorem', :genre_id => '1')
     artist_clone.save
     
     artist_clone.errors.on(:name).should == ("Has already been taken")
@@ -27,14 +27,14 @@ describe Artist do
   end
 
   it "should have many albums" do
-    artist = Factory.create(:name, :genre_id)    
+    artist = Factory.create(:artist)    
     artist.should respond_to(:albums)
   end
 
-  it "should belong to artist" do
-    artist = Factory.build(:name, :genre_id)
+  it "should belong to genre" do
+    artist = Factory.build(:artist)
     artist.save
 
-    artist.should respond_to(:artist)
+    artist.should respond_to(:genre)
   end
 end
