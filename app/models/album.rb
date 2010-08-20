be62fe11353262787be03deb65874ede877d21ed
Gabriel Engel <gabriel@fande.com.br>
2010-08-20 13:54:09 -0300

    Testes de albuns

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,3 @@
 class Album < ActiveRecord::Base
+  validates_presence_of :name, :artist_id
 end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -2,12 +2,17 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Album do
   it "should create" do
-    album = Album.create(:name => "Antonomalia Puris Macabriss")
+    album = Album.create(:name => "Antonomalia Puris Macabriss", :artist_id => 1 )
     album.should be_valid
   end
   
   it "should not create" do
     album = Album.create(:name => "")
+    album.should_not be_valid
+  end
+  
+  it "should not create without artist" do
+    album = Album.create(:name => "Limp Biskit")
     album.should_not be_valid
   end
   
