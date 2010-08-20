015d1b93b5f845f19a99e81c950bf72da71e73b9
Johalf <johalffarina@gmail.com>
2010-08-20 12:08:02 -0300

    testes de artistas passando

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,5 +4,4 @@ class Artist < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
   validates_presence_of :genre_id
-
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -2,28 +2,29 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Artist do
   it 'should create' do
-    artist = Artist.create(:name => "Mayhem")
+    artist = Artist.create(:name => "Mayhem", :genre_id => 1)
     artist.should be_valid
   end
 
   it 'should not create' do
-    artist = Artist.create(:name => "")
+    artist = Artist.create(:name => "", :genre_id => 1)
     artist.should_not be_valid
   end
 
   it 'should have an unique name' do
-    artist = Artist.create(:name => 'Burzum')
-    artist_clone = Artist.create(:name => 'Burzum')
+    artist = Artist.create(:name => 'Burzum', :genre_id => 1)
+    artist_clone = Artist.create(:name => 'Burzum', :genre_id => 1)
     artist.clone.should_not be_valid
   end
   
   it 'should have an unique genre' do
-    artist = Artist.create(:genre_id => '1')
+    artist = Artist.create(:name => "Mayhem", :genre_id => '1')
     artist.should be_valid
   end
   
   it 'should not have an unique genre' do
-    artist = Artist.create(:genre_id => '')
+    artist = Artist.create(:name => "Mayhem", :genre_id => '1')  
+    artist = Artist.create(:name => "Mayhem", :genre_id => '1')
     artist.should_not be_valid
   end
 
