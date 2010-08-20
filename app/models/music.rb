3524de1e85a6a3db3d58ff615383efff9d601b5a
Gabriel Engel <gabriel@fande.com.br>
2010-08-20 14:31:57 -0300

    Agora com testes pra musica

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,18 +1,2 @@
 class Music < ActiveRecord::Base
-  it "should create" do
-    #TODO
-  end
-  
-  it "should not create" do
-    #TODO
-  end
-  
-  it "should not sabe without Album" do
-    #TODO
-  end
-  
-  it "should have a file" do
-    #TODO
-  end
-  
 end
---
+++ spec/models/music_spec.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Music do
+  it "should create" do
+    #TODO
+  end
+  
+  it "should not create" do
+    #TODO
+  end
+  
+  it "should not sabe without Album" do
+    #TODO
+  end
+  
+  it "should have a file" do
+    #TODO
+  end
+end
