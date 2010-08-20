7f8c463dbd4fe23849c52c8b6d857299385f09b2
Letícia <leticia@fande.com.br>
2010-08-20 16:06:32 -0300

    Spec de Artist

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -2,28 +2,36 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Artist do
   it 'should create' do
-    artist = Artist.create(:name => "Mayhem", :genre_id => 1)
+    artist = Factory.create(:name, :artist_id )
     artist.should be_valid
   end
 
   it 'should not create' do
-    artist = Artist.create(:name => "", :genre_id => 1)
+    artist = Factory.create(:name, :artist_id )
+    #artist.save
+
+    artist.errors.on(:name).should == ("Can't be blank")
     artist.should_not be_valid
   end
 
-  it 'should have an unique name and an unique genre' do
-    artist = Artist.create(:name => 'Burzum', :genre_id => 1)
-    artist_clone = Artist.create(:name => 'Burzum', :genre_id => 1)
+  it 'should have an unique name and an unique artist' do
+    artist = Factory.create(:name, :artist_id )
+    #artist.save
+
+    artist_clone = Factory.create(:name, :artist_id)
+    #artist_clone.save
+    artist_clone.errors.on(:name).should == ("Has already been taken")
+
     artist.clone.should_not be_valid
   end
 
   it "should have many albums" do
-    artist = Artist.create(:name => "Mayhem", :genre_id => '1')    
+    artist = Factory.create(:name, :artist_id)    
     artist.should respond_to(:albums)
   end
 
-  it "should belong to genre" do
-    artist = Artist.create(:name => "Mayhem", :genre_id => '1')
-    artist.should respond_to(:genre)
+  it "should belong to artist" do
+    artist = Factory.create(:name, :artist_id)
+    artist.should respond_to(:artist)
   end
 end
