0603421a56aec787f26d1cbf8c6b66ad2963039f
Gabriel Engel <gabriel@fande.com.br>
2010-08-20 15:49:38 -0300

    Adaptando código dos testes para utilizar o FactoryGirl

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -2,23 +2,20 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Album do
   it "should create" do
-    album = Album.create(:name => "Antonomalia Puris Macabriss", :artist_id => 1 )
+    album = Factory.create(:album)
+    #Album.create(:name => "Antonomalia Puris Macabriss", :artist_id => 1 )
     album.should be_valid
   end
   
-  it "should not create" do
-    album = Album.create(:name => "")
+  it "should not create without name" do
+    album = Factory.create(:album, :name=>"")
+    #Album.create(:name => "")
     album.should_not be_valid
   end
   
   it "should not create without artist" do
-    album = Album.create(:name => "Limp Biskit")
+    album = Factory.create(:album, :artist_id=>"")
+    #Album.create(:name => "Limp Biskit")
     album.should_not be_valid
-  end
-  
-  
-  it "should belong to artist" do
-    album = Album.create(:name => "Limp Biskit", :artist_id => 1)
-    album.should respond_to(:artist)
   end
 end
