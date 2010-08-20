cc9ead1c8efa7d5e5932193574fc82abcb32effa
Johalf <johalffarina@gmail.com>
2010-08-20 10:07:04 -0300

    Primeira commitada do projeto, adicionei tudo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bolinari::Application.config.secret_token = '70da546e3ab18dbf90d69984616c5dd51a6412f4c62e06ec819f9a14993cca6c1bac17d3d1d70c2ee961aea817f4f2b7ba8e440107cd10eba8e333beb7e62b49'
